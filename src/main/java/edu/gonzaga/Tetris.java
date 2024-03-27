d2676885abfb33958a132b2a9ed05e4f16dd51ee
Creeper-MZ <e15503075646@hotmail.com>
2024-03-27 16:34:09 -0700

    Change the entry of the program

--- src/main/java/edu/gonzaga/Tetris.java
+++ src/main/java/edu/gonzaga/Tetris.java
@@ -6,16 +6,7 @@ import java.util.Timer;
 
 public class  Tetris {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Long timer= System.currentTimeMillis();
-        GamePad gamePad=new GamePad(70,50);
-        TetroL ter=new TetroL(gamePad.getBlocks(),new Point(5,5));
-        gamePad.addATetro(ter);
-        ter.addToGameListeners(gamePad);
-        while (true){
-            if((System.currentTimeMillis()-timer)%500==0) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-                ter.step();
-            }
-        }
+        TetrisGame game=new TetrisGame();
+        game.startGame();
     }
 }
--- src/main/java/edu/gonzaga/TetrisGame.java
+++ src/main/java/edu/gonzaga/TetrisGame.java
@@ -1,10 +1,23 @@
 package edu.gonzaga;
 
+import java.awt.*;
+import java.io.IOException;
+
 public class TetrisGame {
     public TetrisGame(){
 
     }
-    public void startGame(){
-
+    public void startGame() throws IOException, InterruptedException {
+        Long timer= System.currentTimeMillis();
+        GamePad gamePad=new GamePad(70,50);
+        TetroL ter=new TetroL(gamePad.getBlocks(),new Point(5,5));
+        gamePad.addATetro(ter);
+        ter.addToGameListeners(gamePad);
+        while (true){
+            if((System.currentTimeMillis()-timer)%500==0) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                ter.step();
+            }
+        }
     }
 }
