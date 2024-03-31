16b64805d01d6e08ad78d2f6ee4b36c77ff7f270
jack-345 <jou@zagmail.gonzaga.edu>
2024-03-31 16:02:53 -0700

    added throws exceptions to implement actionListener to mainmenu

--- src/main/java/edu/gonzaga/MainMenuTest.java
+++ src/main/java/edu/gonzaga/MainMenuTest.java
@@ -2,10 +2,16 @@ package edu.gonzaga;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import edu.gonzaga.TetrisGame;
+import edu.gonzaga.Tetris;
 
 public class MainMenuTest {
     
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException, InterruptedException {
         JFrame mainMenuFrame = new JFrame("Main Menu");
         mainMenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainMenuFrame.setSize(770, 820);
@@ -13,6 +19,24 @@ public class MainMenuTest {
         JPanel panel = new JPanel();
         
         JButton playButton = new JButton("Play");
+        playButton.setBounds(50, 100, 150, 30);
+        playButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                TetrisGame game = new TetrisGame();
+                try {
+                    game.startGame();
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                } catch (InterruptedException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+            
+        });
+
+
         JButton settingsButton = new JButton("Settings");
         JButton tutorialButton = new JButton("How To Play");
 
