40546018dbd52d2e2d1f4c7df2658e873bc79968
jack-345 <jou@zagmail.gonzaga.edu>
2024-03-31 17:03:01 -0700

    Update MainMenuTest.java

--- src/main/java/edu/gonzaga/MainMenuTest.java
+++ src/main/java/edu/gonzaga/MainMenuTest.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 import edu.gonzaga.TetrisGame;
 import edu.gonzaga.Tetris;
 
-public class MainMenuTest {
+public class MainMenuTest{
     
     public static void main(String[] args) throws IOException, InterruptedException {
         JFrame mainMenuFrame = new JFrame("Main Menu");
@@ -22,25 +22,30 @@ public class MainMenuTest {
         playButton.setBounds(50, 100, 150, 30);
         playButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                TetrisGame game = new TetrisGame();
-                JFrame application = new JFrame();
-                //opens unresponsive/blank window of TetrisGame
-                try {
-                    game.startGame();
-                } catch (IOException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
-                } catch (InterruptedException e1) {
-                    // TODO Auto-generated catch block
-                    e1.printStackTrace();
+                if (e.getSource() == playButton) {
+                    TetrisGame game = new TetrisGame();
+                    try {
+                        game.startGame();
+                    } catch (IOException | InterruptedException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
                 }
+                
             }
             
         });
 
 
         JButton settingsButton = new JButton("Settings");
+        //TODO: implement settings menu
+
         JButton tutorialButton = new JButton("How To Play");
+        tutorialButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
 
         panel.add(playButton);
         panel.add(settingsButton);
@@ -50,4 +55,5 @@ public class MainMenuTest {
         mainMenuFrame.getContentPane().add(panel);
         mainMenuFrame.setVisible(true);
     }
+
 }
