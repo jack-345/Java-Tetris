42fb841d6e93bcdaa1ef2b8fb5a5cfd42c2d05db
jack-345 <jou@zagmail.gonzaga.edu>
2024-04-02 22:14:53 -0700

    Create TetrisMenu.java

---
+++ src/main/java/edu/gonzaga/TetrisMenu.java
@@ -0,0 +1,22 @@
+package edu.gonzaga;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import edu.gonzaga.TetrisGame;
+import edu.gonzaga.Tetris;
+
+public class TetrisMenu {
+    public static void main(String[] args) {
+        
+    JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(770, 820);
+
+    JPanel panel = new JPanel();
+        
+    }
+}
