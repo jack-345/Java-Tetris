eafda2b4e7b0b6cf96cfaa051cdd3ccd060ac85e
jack-345 <jou@zagmail.gonzaga.edu>
2024-04-04 11:05:02 -0700

    Update TetrisMenu.java

--- src/main/java/edu/gonzaga/TetrisMenu.java
+++ src/main/java/edu/gonzaga/TetrisMenu.java
@@ -10,13 +10,50 @@ import edu.gonzaga.TetrisGame;
 import edu.gonzaga.Tetris;
 
 public class TetrisMenu {
-    public static void main(String[] args) {
-        
-    JFrame frame = new JFrame();
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(770, 820);
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-    JPanel panel = new JPanel();
+        TetrisGame game = new TetrisGame();
+
+        JFrame mainMenuFrame = new JFrame("Main Menu");
+        mainMenuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainMenuFrame.setSize(770, 820);
+        JPanel panel = new JPanel();
         
+        JButton playButton = new JButton("Play");
+        playButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == playButton) {
+                    mainMenuFrame.setVisible(false);
+                    JFrame application = new JFrame();
+                    application.setVisible(true);
+                    try {
+                        game.startGame();
+                    } catch (IOException | InterruptedException e1) {
+                        // TODO Auto-generated catch block
+                        e1.printStackTrace();
+                    }
+
+                }
+            }
+        });
+
+        JButton settingsButton = new JButton("Settings");
+        //TODO: implement settings menu
+
+        JButton tutorialButton = new JButton("How To Play");
+        tutorialButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+
+        panel.add(playButton);
+        panel.add(settingsButton);
+        panel.add(tutorialButton);
+        panel.setLayout(new GridLayout(3,1));
+
+        mainMenuFrame.add(panel);
+        mainMenuFrame.setVisible(true);
+
     }
 }
