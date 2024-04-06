313c27b4387c2afeb03cf4bb72350d07b4dc3a7a
jack-345 <jou@zagmail.gonzaga.edu>
2024-04-06 12:33:01 -0700

    Create Window.java

---
+++ src/main/java/edu/gonzaga/Window.java
@@ -0,0 +1,17 @@
+package edu.gonzaga;
+
+public class Window {
+    public static void main(String[] args) {
+        
+    }
+
+    void splashScreen() {
+
+    }
+
+    void mainMenu() {
+
+    }
+
+    
+}
