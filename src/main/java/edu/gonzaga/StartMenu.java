358c169709795b83c00ae84f018de58ecd7dfebf
Creeper-MZ <e15503075646@hotmail.com>
2024-04-26 15:48:55 -0700

    Update information on StartMenu

--- src/main/java/edu/gonzaga/StartMenu.java
+++ src/main/java/edu/gonzaga/StartMenu.java
@@ -1,6 +1,7 @@
 package edu.gonzaga;
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.plaf.IconUIResource;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,15 +12,17 @@ import java.io.IOException;
 public class StartMenu extends JPanel {
     protected BufferedImage tetrisLogo;
     protected BufferedImage backgroundImage;
-    protected Image startIcon;
-    public JButton startButton;
-    TetrisGame game = new TetrisGame();
+    protected ImageIcon startIcon;
+    protected JButton startButton;
+    protected JLabel nameLabel;
+    protected String info="Tetris Game by [Team Tetris: Jack Ou, Nick DeYoung, Mingze Zhang]";
+    TetrisGame game;
     public StartMenu(TetrisGame game) {
         this.game = game;
         try {
             tetrisLogo = ImageIO.read(new File("src/main/java/edu/gonzaga/SourceImg/TetrisLogo.png"));
             backgroundImage = ImageIO.read(new File("src/main/java/edu/gonzaga/SourceImg/backgroundImg.png"));
-            startIcon = ImageIO.read(new File("src/main/java/edu/gonzaga/SourceImg/StartButton.png"));
+            startIcon = new ImageIcon(ImageIO.read(new File("src/main/java/edu/gonzaga/SourceImg/StartButton.png")));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -31,6 +34,7 @@ public class StartMenu extends JPanel {
         startButton.setLocation(200,400);
         startButton.setVisible(true);
         startButton.setText("Start Game");
+       // startButton.setIcon(startIcon);
         //startButton.setIcon(new ImageIcon(startIcon));
 
         startButton.addActionListener(new ActionListener() {
@@ -50,13 +54,21 @@ public class StartMenu extends JPanel {
            } 
         });
         add(startButton);
+        nameLabel=new JLabel();
+        nameLabel.setLocation(30,650);
+        nameLabel.setSize(600,20);
+        //nameLabel.setText();
+        //nameLabel.setVisible(true);
+      //  add(nameLabel);
     }
     public StartMenu(){
-
+        this(new TetrisGame());
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.drawImage(backgroundImage, 0, 0,500,700, null);
         g.drawImage(tetrisLogo, 50, 0,400,300, null);
+        g.setColor(new Color(255,255,255));
+        g.drawBytes(info.getBytes(),0,info.getBytes().length, 30,650);
     }
 }
